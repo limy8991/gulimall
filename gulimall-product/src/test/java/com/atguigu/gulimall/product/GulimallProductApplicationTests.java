40d9f92afc0770c9a2042255803be5e89e14cae3
myll <limy8991@gmail.com>
2025-06-28 17:15:43 +0800

    base

--- gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
@@ -1,13 +1,30 @@
 package com.atguigu.gulimall.product;
 
+import com.atguigu.gulimall.product.entity.BrandEntity;
+import com.atguigu.gulimall.product.service.BrandService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class GulimallProductApplicationTests {
 
+    @Autowired
+    private BrandService brandService;
+
     @Test
     void contextLoads() {
+        BrandEntity brandEntity = new BrandEntity();
+        brandEntity.setBrandId(1L);
+        brandEntity.setDescript("<UNK>");
+        List<BrandEntity> brandId = brandService.list(new QueryWrapper<>(brandEntity).eq("brand_id", 1L));
+        brandId.forEach((itme)->{
+            System.out.println(itme);
+        });
+        System.out.println(brandEntity.toString());
     }
 
 }
